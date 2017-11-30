d36e2ade262e9f1cbf82e9645852374144d4bd3c
James Nicholas <jelly_island@hotmail.co.nz>
2017-11-30 15:56:42 +1300

    Added filter functionality

--- update.js
+++ update.js
@@ -1,12 +1,12 @@
 let interval = 0
 let display_window = []
 let disply_doc = []
-const def_links = ["https://www.bidbud.co.nz/search?title=Latest+listings+in+Components&search_string=&user_region=100&category=0002-0359-&condition=Used&sort_order=ExpiryDesc&date_from=1509682255&exclude_categories=-0494-3947-0205-1353-0177-0827&saved=1&source=30933"];
-//const filter_terms = ["term1", "term2", ..., "term last"]
+const def_links = ["https://www.bidbud.co.nz/search?exclude_categories=-0494-3842-4250-2924-0091-8729-0356-0357-0358-0362-4570-0363-0364-0360-9844-0244-0043-0397&source=39928&search_string=&category=0002-&user_region=100&sort_order=ExpiryDesc&condition=Used&shipping_method=&suburbs=&min_price=&max_price=&display_type=normal"];
+const filter_terms = ["a", "e", "i", "o", "u"]
 let doc_set = [ false, false ];
 
 // Bid Bud enforces a 100 request per hour limit
-const delay = 74000;
+const delay = 37000;
 
 window.onload = function ()
 {
@@ -15,6 +15,7 @@ window.onload = function ()
     display_window[0].contentDocument.document ||
     display_window[0].contentDocument
   parseRequest();
+  updateFeed();
   interval = setInterval(updateFeed, delay);
 };
 
@@ -50,19 +51,33 @@ function getResultsTableArray( data )
     end = tbody.search("<\/tr>");
 
     trow = tbody.slice( 0, end );
-    next_col = trow.search("<td");
-    cols = [];
-    while( next_col !== -1 )
-    {
-      trow = trow.slice(next_col);
-      trow = trow.slice( trow.search(">") + 1 );
-      end_col = trow.search("<\/td>");
 
-      cols.push( trow.slice( 0, end_col ) );
-      next_col = trow.search("<td");
+    let filter = false
+    for(let i = 0; i < filter_terms.length; i++)
+    {
+        if( trow.toLowerCase().search(filter_terms[i].toLowerCase()) !== -1 )
+        {
+          filter = true
+          break
+        }
     }
 
-    entries.push( cols );
+    if (!filter)
+    {
+      next_col = trow.search("<td");
+      cols = [];
+      while( next_col !== -1 )
+      {
+        trow = trow.slice(next_col);
+        trow = trow.slice( trow.search(">") + 1 );
+        end_col = trow.search("<\/td>");
+
+        cols.push( trow.slice( 0, end_col ) );
+        next_col = trow.search("<td");
+      }
+
+      entries.push( cols );
+    }
     next = tbody.search("<tr");
   }
   return entries;
